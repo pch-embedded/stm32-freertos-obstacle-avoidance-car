5254d1003a761242a950a4d9e0d2a55210b1703e
pch <pch@example.com>
2026-04-11 22:34:22 +0900

    수정: 좌우 회전 방향 정상화

--- Core/Src/motor.c
+++ Core/Src/motor.c
@@ -57,29 +57,29 @@ void Motor_TurnLeft(uint8_t step)
 {
 	if (!s_htim) return;
 	/* 왼쪽 바퀴 정지, 오른쪽 바퀴 전진*/
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
 
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
 	 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
 
 	 uint16_t duty = (step <3) ? speed_table[step] : 0;
 
-	 __HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_1, 0);
-	 __HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_2, duty);
+	 __HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_1, duty);
+	 __HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_2, 0);
 }
 
 void Motor_TurnRight(uint8_t step) {
 	if (!s_htim) return;
 
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
 
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
 
 	uint16_t duty = (step < 3) ? speed_table[step] : 0;
 
-	__HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_1, duty);  // 한쪽 바퀴 전진
-	__HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_2, 0);     // 다른쪽 바퀴 정지
+	__HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_1, 0);  // 한쪽 바퀴 전진
+	__HAL_TIM_SET_COMPARE(s_htim, TIM_CHANNEL_2, duty);     // 다른쪽 바퀴 정지
 }
